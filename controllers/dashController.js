a9d8c2d3a1d8d348fb394895b0ce9e5cecabcce1
epixieme <kirstie_hayes@hotmail.com>
2022-09-22 16:06:59 +0100

    added tools screen for app

--- controllers/dashController.js
+++ controllers/dashController.js
@@ -2,7 +2,10 @@ module.exports = {
     getDashboard: async (req,res)=>{
         console.log(req.user)
         try{
-          res.render('dashboard')
+          res.render('dashboard',{
+            title: "Dashboard",
+            layout:'./layouts/dashboard-home.ejs'
+          })
             // res.render('todos.ejs', {todos: todoItems, left: itemsLeft, user: req.user})
         }catch(err){
             console.log(err)
