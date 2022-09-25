e50e0fc8e3dbd886f1712b962755d76977f49a73
epixieme <kirstie_hayes@hotmail.com>
2022-09-25 11:41:49 +0100

    added routes and controllers, ejs for dashboard thoughts

--- controllers/dashController.js
+++ controllers/dashController.js
@@ -1,10 +1,19 @@
+const thoughtDiary = require("../models/Thoughts");
+const userDetails = require("../models/User");
+
 module.exports = {
     getDashboard: async (req,res)=>{
         console.log(req.user)
         try{
+          const thoughts = await thoughtDiary.find({ user: req.user.id }).sort({submitted: -1}).limit(7)
+          const users = await userDetails.find();
+
           res.render('dashboard',{
             title: "Dashboard",
-            layout:'./layouts/dashboard-home.ejs'
+            layout:'./layouts/dashboard-home.ejs',
+            thoughts:thoughts,
+            users:users,
+            user:req.user
           })
      
         }catch(err){
