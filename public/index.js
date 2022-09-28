b6b7f3497a6719546158b645419803d3ce30913c
epixieme <kirstie_hayes@hotmail.com>
2022-09-28 14:26:16 +0100

    added code for showing dates on thoughts page

--- public/index.js
+++ public/index.js
@@ -14,7 +14,6 @@ if(slider){
   slider.addEventListener('input', feelings)
 }
 
-
 const emotions = {
 
 0:"Great",
@@ -25,47 +24,74 @@ const emotions = {
 5:"Awful"
 }
 
-
-
 function feelings(){
 
   // removes undefined values which were introduced when the range slider step was changed to .1
  slideText.value =  JSON.parse(JSON.stringify(emotions[this.value]))
 // console.log(slideText)
  moodRating.value = JSON.parse(JSON.stringify(this.value))
- console.log(moodRating.value)
-}
-
-function createDatesArray(){
-
-let array = [1,2,3,4,5,6,7]
-const dateArray = Array.from(dates)
-let store =[]
-for (i=0;i<array.length;i++){
-store.push(dateArray[i])
-}
-return store.map(v => v === undefined ? '-' : v).filter(item=>item === '-')
 
 }
 
-function insertDatesArray(){
-let array = createDatesArray()
-console.log(array)
+// function createDatesArray(){
 
-for(i=0;i<array.length;i++){
- const list = document.createElement('li')
+// let array = [1,2,3,4,5,6,7]
+// const dateArray = [...dates]
+// let store =[]
+
+// let formatD = dateArray.map(item=>{
+//  let formattedD =  new Date(item.textContent)
+// let shortenDate = `${formattedD.getDate()}/${formattedD.getMonth()+ 1}`
+// return shortenDate
+// })
+
+// for (i=0;i<array.length;i++){
  
- mongoDates.appendChild(list)
- list.innerHTML ='-'
 
-}
-return dates
+//   // let formatDate = `${dateArray.getDate()}/${dateArray.getMonth()+ 1}`
+// store.push(formatD[i])
+// }
+// return store.map(value => value === undefined ? '-' : value)
 
-}
-
-insertDatesArray()
+// }
 
 
+// function insertDatesArray(){
+// let array = createDatesArray()
+
+// let dashes = array.filter(item=>item === '-')
+// let dateValues = array.filter(item=>item !== '-')
+// console.log(dashes, dateValues)
+
+// for(i=0;i<array.length;i++){
+//   dates[i].innerHTML = dateValues[i]
+//   let dashLi = document.createElement('li')
+//   dashLi.innerHTML=dashes[i]
+//   mongoDates.appendChild(dashLi)
+//   }
+
+
+// // }
+// return dates
+
+// }
+
+// insertDatesArray()
 
 
 
+
+
+// const past7Days = [...Array(7).keys()].map(index => {
+//   const date = new Date();
+//   date.setDate(date.getDate() - index);
+//   return `${date.getDate()}/${date.getMonth()+ 1}`;
+// });
+
+
+
+// module.exports = {
+//   // reverse the dates to show past to present
+//   past7Days: past7Days.reverse(),
+ 
+// }
