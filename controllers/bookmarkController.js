016eb5b90e7cda73a5aac094bd20775aa560e654
epixieme <kirstie_hayes@hotmail.com>
2022-10-21 14:01:16 +0100

    adding stylig and modals

--- controllers/bookmarkController.js
+++ controllers/bookmarkController.js
@@ -1,5 +1,6 @@
 const thoughtDiary = require("../models/Thoughts");
 const userDetails = require("../models/User");
+const motivationDetails = require("../models/Motivations");
 function errorHandling(res, error) {
     res.status(500).send({ message: error.message || "Error Occured" });
   }
@@ -20,6 +21,21 @@ module.exports = {
         errorHandling(res, err)
     }
 },
+addMotivationBookmark: async (req, res)=>{
+
+    console.log('this is id' + req.params.id)
+try{
+    const motivations = await motivationDetails.findOneAndUpdate({_id:req.params.id},{
+        bookmarked: true
+    })
+    console.log('bookmarked')
+   
+    res.redirect("/dashboard/tools/motivations");
+}catch(err){
+
+    errorHandling(res, err)
+}
+},
 removeBookmark: async (req, res)=>{
     try{
         const thoughts = await thoughtDiary.findOneAndUpdate({_id:req.params.id},{
@@ -37,11 +53,15 @@ getBookmarked: async (req, res)=>{
         const thoughts = await thoughtDiary.find({
             bookmarked:true}
         )
+        const motivations = await motivationDetails.find({
+            bookmarked:true}
+        )
         console.log('bookmarked')
         res.render('bookmarks',{
             title: "Bookmarks",
             layout:'./layouts/dashboard-home.ejs',
             thoughts:thoughts,
+            motivations,motivations,
             user:req.user
           })
     }catch(err){
